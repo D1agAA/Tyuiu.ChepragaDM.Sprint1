62a06cf4140898cf3dfd00631a9b1b095e98c5ff
DiagAA 69 <dima.prosto.2019@inbox.ru>
2025-09-22 14:53:00 +0500

    Task0

--- Tyuiu.ChepragaDM.Sprint1.Task0.V7.Lib/DataService.cs
+++ Tyuiu.ChepragaDM.Sprint1.Task0.V7.Lib/DataService.cs
@@ -1,7 +1,7 @@
 ﻿using tyuiu.cources.programming.interfaces.Sprint1;
 namespace Tyuiu.ChepragaDM.Sprint1.Task0.V17.Lib
 {
-    public class DataService : ISprint1Task0V17
+    public class DataService : ISprint1Task0V7
     {
         public double Calculate()
         {
