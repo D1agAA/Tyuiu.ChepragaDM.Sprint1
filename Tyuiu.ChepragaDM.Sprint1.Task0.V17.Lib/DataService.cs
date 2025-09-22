7697ae20f6040d70c6d4646fb8916869632ba09f
DiagAA 69 <dima.prosto.2019@inbox.ru>
2025-09-22 14:17:31 +0500

    Add project files.

---
+++ Tyuiu.ChepragaDM.Sprint1.Task0.V17.Lib/DataService.cs
@@ -0,0 +1,11 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint1;
+namespace Tyuiu.ChepragaDM.Sprint1.Task0.V17.Lib
+{
+    public class DataService : ISprint1Task0V17
+    {
+        public double Calculate()
+        {
+            return 10 / (2 + 3);
+        }
+    }
+}
---
+++ Tyuiu.ChepragaDM.Sprint1.Task0.V17.Test/DataServiceTest.cs
@@ -0,0 +1,15 @@
+﻿using Tyuiu.ChepragaDM.Sprint1.Task0.V17.Lib; 
+namespace Tyuiu.ChepragaDM.Sprint1.Task0.V17.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            var res = ds.Calculate();
+            Assert.AreEqual(2, res);
+        }
+    }
+}
