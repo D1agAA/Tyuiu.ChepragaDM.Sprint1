59cc290dddd2f98d0bcde887f3ca2e46a9d5de93
DiagAA 69 <dima.prosto.2019@inbox.ru>
2025-09-30 22:02:55 +0500

    Task4

---
+++ Tyuiu.ChepragaDM.Sprint1.Task4.V8.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Tyuiu.ChepragaDM.Sprint1.Task4.V8.Lib;
+namespace Tyuiu.ChepragaDM.Sprint1.Task4.V8.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+
+            DataService ds = new DataService();
+            var x = 1;
+            var y = 2;
+            var res = ds.Calculate(x, y);
+            Assert.AreEqual(1, res);
+
+        }
+    }
+}
---
+++ Tyuiu.ChepragaDM.Sprint1.Task4.V8/Program.cs
@@ -0,0 +1,43 @@
+﻿using Tyuiu.ChepragaDM.Sprint1.Task4.V8.Lib;
+namespace Tyuiu.ChepragaDM.Sprint1.Task4.V8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #1 | Выполнил: Чепрага Д. М. | ИБКСб-25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Class Math                                                        *");
+            Console.WriteLine("* Задание #4                                                              *");
+            Console.WriteLine("* Вариант #8                                                              *");
+            Console.WriteLine("* Выполнил: Чепрага Дмитрий Максимович | ИБКСб-25-1                       *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные, *");
+            Console.WriteLine("* выполняет указанные расчёты и печатает результат на экране              *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            double x, y;
+
+            Console.WriteLine("Введите значение Х: ");
+            x = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите значение Y: ");
+            y = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine(ds.Calculate(x, y));
+
+            Console.ReadKey();
+        }
+    }
+}
