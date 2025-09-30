73baa95680089dfac2dd698d8c8a404decb64c65
DiagAA 69 <dima.prosto.2019@inbox.ru>
2025-09-30 21:51:51 +0500

    Task2

---
+++ Tyuiu.ChepragaDM.Sprint1.Task2.V23.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.ChepragaDM.Sprint1.Task2.V23.Lib;
+namespace Tyuiu.ChepragaDM.Sprint1.Task2.V23.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int x = 2;
+            var res = ds.ConvertMinutesToSeconds(x);
+            Assert.AreEqual(120, res);
+
+        }
+    }
+}
---
+++ Tyuiu.ChepragaDM.Sprint1.Task2.V23/Program.cs
@@ -0,0 +1,43 @@
+﻿using Tyuiu.ChepragaDM.Sprint1.Task2.V23.Lib;
+namespace Tyuiu.ChepragaDM.Sprint1.Task2.V23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #1 | Выполнил: Чепрага Д. М. | ИБКСб-25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Арифметические операторы в С#                                     *");
+            Console.WriteLine("* Задание #2                                                              *");
+            Console.WriteLine("* Вариант #23                                                             *");
+            Console.WriteLine("* Выполнил: Чепрага Дмитрий Максимович | ИБКСб-25-1                       *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные  *");
+            Console.WriteLine("* выполняет указанные расчёты и печатает результат на экране.             *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int x;
+
+            Console.WriteLine("Введите количество минут: ");
+            x = Convert.ToInt32(Console.ReadLine());
+
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine(x + " минут = " + ds.ConvertMinutesToSeconds(x) + " секунд");
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
