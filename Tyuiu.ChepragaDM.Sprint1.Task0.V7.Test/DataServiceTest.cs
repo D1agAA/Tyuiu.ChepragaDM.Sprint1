1cadcf650091562f599dfe49c0658196673db4be
DiagAA 69 <dima.prosto.2019@inbox.ru>
2025-09-30 21:41:24 +0500

    Add project files.

---
+++ Tyuiu.ChepragaDM.Sprint1.Task0.V7.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.ChepragaDM.Sprint1.Task0.V7.Lib;
+namespace Tyuiu.ChepragaDM.Sprint1.Task0.V7.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            var res = ds.Calculate();
+
+            Assert.AreEqual(5, res);
+        }
+    }
+}
+
