4b892717c49dfa945b97c1ff88f584d96b4ff288
DiagAA 69 <dima.prosto.2019@inbox.ru>
2025-09-30 23:25:27 +0500

    Task5

---
+++ Tyuiu.ChepragaDM.Sprint1.Task5.V1.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Tyuiu.ChepragaDM.Sprint1.Task5.V1.Lib;
+namespace Tyuiu.ChepragaDM.Sprint1.Task5.V1.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ExpressionValid()
+        {
+            DataService ds = new DataService();
+            double x1 = 1.0;
+            double y1 = 1.0;
+            double x2 = 5.0;
+            double y2 = 4.0;
+            var res = ds.DistanceBetweenDots(x1, y1, x2, y2);
+            Assert.AreEqual(5, res);
+        }
+    }
+}
---
+++ Tyuiu.ChepragaDM.Sprint1.Task5.V1/Program.cs
@@ -0,0 +1,50 @@
+﻿using Tyuiu.ChepragaDM.Sprint1.Task5.V1.Lib;
+namespace Tyuiu.ChepragaDM.Sprint1.Task5.V1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #1 | Выполнил: Чепрага Д. М. | ИБКСб-25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Преобразование типов в класс Convert                              *");
+            Console.WriteLine("* Задание #5                                                              *");
+            Console.WriteLine("* Вариант #1                                                              *");
+            Console.WriteLine("* Выполнил: Чепрага Дмитрий Максимович | ИБКСб-25-1                       *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая решает следующую задачу:                    *");
+            Console.WriteLine("* Найти расстояние между двумя точками с заданными координатами (x, y).   *");
+            Console.WriteLine("* Ответ привести к целому с помощью класса Convert.                       *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            double x1, y1, x2, y2;
+
+            Console.WriteLine("Введите значение Х1: ");
+            x1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите значение Y1: ");
+            y1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите значение X2: ");
+            x2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите значение Y2: ");
+            y2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine(ds.DistanceBetweenDots(x1, y1,x2,y2));
+
+            Console.ReadKey();
+        }
+    }
+}
