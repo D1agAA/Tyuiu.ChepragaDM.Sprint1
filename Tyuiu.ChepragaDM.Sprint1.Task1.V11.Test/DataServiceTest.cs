e5ec3bcf2b84e72760d650fa5f6addc9d5f88359
DiagAA 69 <dima.prosto.2019@inbox.ru>
2025-09-30 21:48:17 +0500

    Task1

---
+++ Tyuiu.ChepragaDM.Sprint1.Task1.V11.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.ChepragaDM.Sprint1.Task1.V11.Lib;
+namespace Tyuiu.ChepragaDM.Sprint1.Task1.V11.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            double x = 6.0;
+            double y = 1.0;
+            var res = ds.Calculate(x, y);
+            Assert.AreEqual(5.0, res);
+        }
+    }
+}
+
---
+++ Tyuiu.ChepragaDM.Sprint1.Task1.V11/Program.cs
@@ -0,0 +1,46 @@
+﻿using Tyuiu.ChepragaDM.Sprint1.Task1.V11.Lib;
+namespace Tyuiu.ChepragaDM.Sprint1.Task1.V11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #1 | Выполнил: Чепрага Д. М. | ИБКСб-25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Организация ввода или вывода в консольных приложениях             *");
+            Console.WriteLine("* Задание #1                                                              *");
+            Console.WriteLine("* Вариант #11                                                             *");
+            Console.WriteLine("* Выполнил: Чепрага Дмитрий Максимович | ИБКСб-25-1                       *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные         *");
+            Console.WriteLine("* данные, вычисляет результат по формуле 5 * x / (6 * y) и печатает его   *");
+            Console.WriteLine("* на экране.                                                              *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            double x, y;
+
+            Console.WriteLine("Введите чисто Х: ");
+            x = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите чисто Y: ");
+            y = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine(ds.Calculate(x, y));
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
